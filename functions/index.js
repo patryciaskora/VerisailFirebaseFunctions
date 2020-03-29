4435ecf6479ab943d58a80fa6ec55dbb7d966d5e
patryciaskora <s1105923@monmouth.edu>
2020-03-29 15:02:47 -0400

    JSON parsing done, need to test POST and expose URL

--- functions/index.js
+++ functions/index.js
@@ -7,14 +7,14 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
 
-exports.readInspections = functions.https.onRequest((request, response) => {
-    var allInspections = admin.database().ref("event_inspections");
-    allInspections.on("value", gotData, gotErr); 
+exports.readInspectionsDetails = functions.https.onRequest((request, response) => {
+    var allInspectionDetails = admin.database().ref("event_inspections");
+    allInspectionDetails.on("value", gotData, gotErr); 
 
     function gotData(snapshot) {
-      var inspections = snapshot.val();
+      var inspectionDetails = snapshot.val();
       var numEvents = snapshot.numChildren();
-      var eventIDS = Object.keys(inspections)
+      var eventIDS = Object.keys(inspectionDetails)
 
       // all events
       snapshot.forEach(function(eventIDSnapshot) {
@@ -24,23 +24,62 @@ exports.readInspections = functions.https.onRequest((request, response) => {
         // all dates
         eventIDSnapshot.forEach(function(dateIDSnapshot) {
           var date = dateIDSnapshot.key;
-          var dateData = dateIDSnapshot.val();
           // all boats
           dateIDSnapshot.forEach(function(boatIDSnapshot) {
             var boatID = boatIDSnapshot.key;
-            var boatIDData = boatIDSnapshot.val();
-
             var insp = {"eventID": eventID, "boatID": boatID};
             console.log("INSPECTION: " + JSON.stringify(insp)); 
+
+            //all parts for each boat
             boatIDSnapshot.forEach(function(inspectionDetailsSnapshot) {
-              console.log(inspectionDetailsSnapshot.val());
+              var part = inspectionDetailsSnapshot.key;
+              var partDetails = inspectionDetailsSnapshot.val();
+
+              partDetails.eventID = eventID;
+              partDetails.date = date;
+
+              console.log("inspection detail: " +  JSON.stringify(partDetails));
+
+              response.send(JSON.stringify(partDetails));
             })
           })
         })
       })
     }
-
     function gotErr(snapshot){
       console.log("Got error");
     }
+});
+
+exports.readInspection = functions.https.onRequest((request, response) => {
+  var allInspections = admin.database().ref("event_registrations");
+  allInspections.on("value", gotData, gotErr);
+
+  function gotData(snapshot) {
+    var inspections = snapshot.val();
+    var numEvents = snapshot.numChildren();
+    var eventIDS = Object.keys(inspections)
+
+    snapshot.forEach(function(eventIDSnapshot) {
+      var eventID = eventIDSnapshot.key;
+      var eventIDData = eventIDSnapshot.val();
+
+      eventIDSnapshot.forEach(function(boatIDSnapshot) {
+        var boatID = boatIDSnapshot.key;
+        var boatIDData = boatIDSnapshot.val();
+
+        boatIDData.eventID = eventID;
+        boatIDData.boatID = boatID;
+
+        console.log("boat id data" + JSON.stringify(boatIDData));
+
+        var dataBody = JSON.stringify(boatIDData);
+
+        response.send(dataBody);
+      })
+    })
+  }
+  function gotErr(snapshot){
+    console.log("Got an error");
+  }
 });
